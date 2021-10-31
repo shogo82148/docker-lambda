492993a33231365fad484601e69a1ae0f65a15bf
Ichinose Shogo <shogo82148@gmail.com>
2021-10-31 22:56:12 +0900

    add provided and nodejs repository

--- dockerfiles/base-2/publish.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-base:2 .
---
+++ dockerfiles/nodejs14.x/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker buildx build --platform linux/amd64,linux/arm64 -t public.ecr.aws/shogo82148/lambda-nodejs:14 "$CURRENT/run"
