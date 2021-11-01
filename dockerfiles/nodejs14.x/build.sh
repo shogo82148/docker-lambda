ac14fcc30da910459f67c13912b7e7b71c634b26
Ichinose Shogo <shogo82148@gmail.com>
2021-11-02 07:53:20 +0900

    introduce build-nodejs14

--- dockerfiles/nodejs14.x/build.sh
+++ dockerfiles/nodejs14.x/build.sh
@@ -3,3 +3,4 @@
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
 docker buildx build --platform linux/amd64,linux/arm64 -t public.ecr.aws/shogo82148/lambda-nodejs:14 "$CURRENT/run"
+docker buildx build --platform linux/amd64,linux/arm64 -t public.ecr.aws/shogo82148/lambda-nodejs:build-14 "$CURRENT/build"
