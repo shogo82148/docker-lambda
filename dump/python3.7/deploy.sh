952efbd207aceefc8a73de9c96ad1f711386db8a
Ichinose Shogo <shogo82148@gmail.com>
2021-11-02 23:46:35 +0900

    migrate Python 3.6 and 3.7 runtime

---
+++ dump/python3.7/deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eux
+
+ARCH=$1
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+
+# upload the code
+BUCKET=$(aws cloudformation describe-stacks \
+    --region us-east-1 \
+    --stack-name s3 \
+    --output text \
+    --query "Stacks[0].Outputs[?OutputKey=='Bucket'] | [0].OutputValue")
+DIGEST=$(openssl sha256 < "$CURRENT/dist.zip")
+KEY=code/python3.7/$DIGEST
+aws s3 cp "$CURRENT/dist.zip" "s3://$BUCKET/$KEY"
+
+# get the arn of dump layer
+LAYER=$(aws cloudformation describe-stacks \
+    --region us-east-1 \
+    --stack-name "docker-lambda-dump-${ARCH/_/-}" \
+    --output text \
+    --query "Stacks[0].Outputs[?OutputKey=='DumpLayer'] | [0].OutputValue")
+
+aws cloudformation deploy \
+    --region us-east-1 \
+    --stack-name "lambda-dump-python37-${ARCH/_/-}" \
+    --parameter-overrides \
+        "Name=dump-python37-${ARCH/_/-}" \
+        "DumpLayer=$LAYER" \
+        "S3Bucket=$BUCKET" \
+        "S3Key=$KEY" \
+        "Architecture=$ARCH" \
+    --template-file "${CURRENT}/template.yaml" \
+    --capabilities CAPABILITY_IAM
---
+++ dump/python3.7/index.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import subprocess
+import json
+
+def handler(event, context):
+    bucket = os.environ['BUCKET']
+    subprocess.call(['sh', '-c', f'lambda-dump -bucket {bucket} -key fs/__ARCH__/python3.7.tgz'])
+
+    info = {
+        'sys.executable': sys.executable,
+        'sys.argv': sys.argv,
+        'sys.path': sys.path,
+        'os.getcwd': os.getcwd(),
+        '__file__': __file__,
+        'os.environ': {k: str(v) for k, v in os.environ.items()},
+        'context': {k: str(v) for k, v in context.__dict__.items()},
+    }
+
+    print(json.dumps(info, indent=2))
+
+    return {}
