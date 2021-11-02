a0fc01836caef3abb0a8e54a86e605032f664f2b
Ichinose Shogo <shogo82148@gmail.com>
2021-11-03 00:14:23 +0900

    dump ruby2.7

---
+++ dump/ruby2.7/index.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+def handler(event:, context:)
+  bucket = ENV['BUCKET']
+  puts `lambda-dump -bucket #{bucket} -key fs/__ARCH__/ruby2.7.tgz`
+
+  info = {
+    'ENV' => ENV.to_hash,
+    'context' => context.instance_variables.each_with_object({}) { |k, h| h[k] = context.instance_variable_get k },
+  }
+
+  print JSON.pretty_generate(info)
+
+  return info
+end
