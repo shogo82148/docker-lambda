c07d45276ad3d168376beb22bf9c9c18b6edd932
Ichinose Shogo <shogo82148@gmail.com>
2021-11-03 15:12:19 +0900

    migrate dump java8

--- dump/java8/src/main/java/com/shogo82148/lambda/DumpJava8.java
+++ dump/java8/src/main/java/com/shogo82148/lambda/DumpJava8.java
@@ -1,4 +1,4 @@
-package org.lambci.lambda;
+package com.shogo82148.lambda;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
@@ -7,20 +7,11 @@ import java.util.Scanner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
 
-public class DumpJava8 implements RequestHandler<Object, PutObjectResult> {
+public class DumpJava8 implements RequestHandler<Object, Object> {
 
     @Override
-    public PutObjectResult handleRequest(Object input, Context context) {
-        String filename = "java8.tgz";
-        String cmd = "tar -cpzf /tmp/" + filename + " --numeric-owner --ignore-failed-read /var/runtime /var/lang";
-        AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion("us-east-1").build();
-
+    public Object handleRequest(Object input, Context context) {
         System.out.println(ManagementFactory.getRuntimeMXBean().getInputArguments().toString());
         System.out.println(System.getProperty("sun.java.command"));
         System.out.println(System.getProperty("java.home"));
@@ -34,6 +25,9 @@ public class DumpJava8 implements RequestHandler<Object, PutObjectResult> {
         for (String envName : env.keySet()) {
             System.out.println(envName + "=" + env.get(envName));
         }
+
+        String filename = "java11.tgz";
+        String cmd = "lambda-dump -bucket " + env.get("BUCKET") + " -key fs/__ARCH__/" + filename;
 
         try {
             Process process = Runtime.getRuntime().exec(new String[] { "sh", "-c", cmd });
@@ -56,11 +50,9 @@ public class DumpJava8 implements RequestHandler<Object, PutObjectResult> {
             }
 
             System.out.println("Zipping done! Uploading...");
-
-            return s3client.putObject(new PutObjectRequest("lambci", "fs/" + filename, new File("/tmp/" + filename))
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        return new Object();
     }
 }
