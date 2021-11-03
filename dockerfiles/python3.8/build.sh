5ccc506975a5364b9221143ce602e5ca0929dd66
Ichinose Shogo <shogo82148@gmail.com>
2021-11-03 22:34:44 +0900

    migrate python3.8 runtime

---
+++ dockerfiles/python3.8/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eux
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker buildx build --platform linux/amd64,linux/arm64 -t public.ecr.aws/shogo82148/lambda-python:3.8 "$CURRENT/run"
+docker buildx build --platform linux/amd64,linux/arm64 -t public.ecr.aws/shogo82148/lambda-python:build-3.8 "$CURRENT/build"
