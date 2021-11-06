98b33746fe38c10abe7a26e3c01998061aed742c
Ichinose Shogo <shogo82148@gmail.com>
2021-11-06 13:20:08 +0900

    fix tag name for dotnectore3.1 runtime

--- dockerfiles/dotnetcore3.1/publish.sh
+++ dockerfiles/dotnetcore3.1/publish.sh
@@ -2,5 +2,5 @@
 
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-ruby:3.1 "$CURRENT/run"
-docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-ruby:build-3.1 "$CURRENT/build"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-dotnetcore:3.1 "$CURRENT/run"
+docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-dotnetcore:build-3.1 "$CURRENT/build"
