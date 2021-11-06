9ddf941d06d3e22100f1a5542e4440e96ab67619
Ichinose Shogo <shogo82148@gmail.com>
2021-11-06 13:48:28 +0900

    introduce CI build

--- dump/templates/deploy.sh
+++ dump/templates/deploy.sh
@@ -8,3 +8,9 @@ aws cloudformation deploy \
     --region us-east-1 \
     --stack-name "s3" \
     --template-file "${CURRENT}/s3.yaml"
+
+aws cloudformation deploy \
+    --region us-east-1 \
+    --stack-name "github-actions" \
+    --template-file "${CURRENT}/github-actions.yaml" \
+    --capabilities CAPABILITY_NAMED_IAM
