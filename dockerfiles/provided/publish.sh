f3260ecddf65bbc0f654ea6381b7524f0dcec626
Ichinose Shogo <shogo82148@gmail.com>
2021-11-13 22:09:15 +0900

    fix provided image

--- dockerfiles/provided/publish.sh
+++ dockerfiles/provided/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eux
+set -e
+
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-provided:alami "$CURRENT/run"
-docker buildx build --platform linux/amd64,linux/arm64 --push -t public.ecr.aws/shogo82148/lambda-provided:build-alami "$CURRENT/build"
+"$CURRENT/../scripts/publish.sh" provided provided alami
