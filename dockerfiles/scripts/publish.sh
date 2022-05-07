5348c872398b6a388c66d6f302d9ee9030e21cb3
Ichinose Shogo <shogo82148@gmail.com>
2022-05-07 10:34:16 +0900

    publish images to ghcr.io

--- dockerfiles/scripts/publish.sh
+++ dockerfiles/scripts/publish.sh
@@ -6,13 +6,19 @@ RUNTIME=$1
 LANGUAGE=$2
 VERSION=$3
 
-RUN_TAG=public.ecr.aws/shogo82148/lambda-$LANGUAGE:$VERSION
-BUILD_TAG=public.ecr.aws/shogo82148/lambda-$LANGUAGE:build-$VERSION
+publish() {
+    local PREFIX=$1
+    local RUN_TAG=$PREFIX-$LANGUAGE:$VERSION
+    local BUILD_TAG=$PREFIX-$LANGUAGE:build-$VERSION
 
-docker buildx build --platform linux/amd64 --push -t "$RUN_TAG" "$ROOT/$RUNTIME/run"
-docker buildx build --platform linux/amd64 --push -t "$BUILD_TAG" "$ROOT/$RUNTIME/build"
+    docker buildx build --platform linux/amd64 --push -t "$RUN_TAG" "$ROOT/$RUNTIME/run"
+    docker buildx build --platform linux/amd64 --push -t "$BUILD_TAG" "$ROOT/$RUNTIME/build"
 
-if [[ -n ${LAMBDA_IMAGE_VERSION:-} ]]; then
-    docker buildx build --platform linux/amd64 --push -t "$RUN_TAG.$LAMBDA_IMAGE_VERSION" "$ROOT/$RUNTIME/run"
-    docker buildx build --platform linux/amd64 --push -t "$BUILD_TAG.$LAMBDA_IMAGE_VERSION" "$ROOT/$RUNTIME/build"
-fi
+    if [[ -n ${LAMBDA_IMAGE_VERSION:-} ]]; then
+        docker buildx build --platform linux/amd64 --push -t "$RUN_TAG.$LAMBDA_IMAGE_VERSION" "$ROOT/$RUNTIME/run"
+        docker buildx build --platform linux/amd64 --push -t "$BUILD_TAG.$LAMBDA_IMAGE_VERSION" "$ROOT/$RUNTIME/build"
+    fi
+}
+
+publish public.ecr.aws/shogo82148/lambda
+publish ghcr.io/shogo82148/lambda
