f24c37adcf9096823359fe4e85c07e480a707d1f
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-24 05:33:56 +0900

    update archive url of dotnet6

---
+++ dockerfiles/dotnet6/run/update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+cd "$CURRENT"
+
+# fetch the latest base image
+METADATA_AMD64=$(curl -sSL --retry 3 "https://shogo82148-docker-lambda.s3.amazonaws.com/fs/x86_64/dotnet6.json")
+ARCHIVE_URL_AMD64=$(echo "$METADATA_AMD64" | jq -r .url)
+
+METADATA_ARM64=$(curl -sSL --retry 3 "https://shogo82148-docker-lambda.s3.amazonaws.com/fs/arm64/dotnet6.json")
+ARCHIVE_URL_ARM64=$(echo "$METADATA_ARM64" | jq -r .url)
+
+export ARCHIVE_URL_AMD64
+export ARCHIVE_URL_ARM64
+
+# update Dockerfile
+perl -pi -e 's/^ENV ARCHIVE_URL_AMD64=.*$/ENV ARCHIVE_URL_AMD64="$ENV{ARCHIVE_URL_AMD64}"/g' Dockerfile
+perl -pi -e 's/^ENV ARCHIVE_URL_ARM64=.*$/ENV ARCHIVE_URL_ARM64="$ENV{ARCHIVE_URL_ARM64}"/g' Dockerfile
