e701a7a081f49fed776ee351ce22d50cf247c807
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-25 00:39:16 +0900

    Delete build and publish scripts for base and base.al2 images

--- dockerfiles/base.al2/build-run.sh
+++ dockerfiles/base.al2/build-run.sh
@@ -3,4 +3,3 @@
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
 docker buildx build --platform linux/amd64 --load -t alami "$CURRENT/run"
-docker buildx build --platform linux/amd64 --load -t build-alami "$CURRENT/build"
