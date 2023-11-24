8d07828ea2bf4139576b7731ca8e7eddff3cc637
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-25 06:10:23 +0900

    publish to ECR Public

--- dockerfiles/base/update.sh
+++ dockerfiles/base/update.sh
@@ -12,3 +12,8 @@ export ARCHIVE_URL
 
 # update Dockerfile
 perl -pi -e 's/^ENV ARCHIVE_URL=.*$/ENV ARCHIVE_URL="$ENV{ARCHIVE_URL}"/g' run/Dockerfile
+
+# fetch the latest base image
+BASE_RUN=$(gh api --jq '[.[].ref] | sort | last' /repos/shogo82148/docker-lambda/git/matching-refs/tags/base-run/ | cut -d/ -f4)
+
+perl -pi -e 's(^FROM public.ecr.aws/shogo82148/lambda-base:alami[0-9.]+$)(FROM public.ecr.aws/shogo82148/lambda-base:alami.'$BASE_RUN')' build/Dockerfile
