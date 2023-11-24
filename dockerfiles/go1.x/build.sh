d993a64a12d750d2188105bc9474b29b8d2ac13a
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-24 21:18:13 +0900

    Fix Docker build commands

--- dockerfiles/go1.x/build.sh
+++ dockerfiles/go1.x/build.sh
@@ -2,5 +2,5 @@
 
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64 -t public.ecr.aws/shogo82148/lambda-go:1 "$CURRENT/run"
-docker buildx build --platform linux/amd64 -t public.ecr.aws/shogo82148/lambda-go:build-1 "$CURRENT/build"
+docker buildx build --platform linux/amd64 --load -t public.ecr.aws/shogo82148/lambda-go:1 "$CURRENT/run"
+docker buildx build --platform linux/amd64 --load -t public.ecr.aws/shogo82148/lambda-go:build-1 "$CURRENT/build"
