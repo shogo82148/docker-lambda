f902b863432c5f99848ac403a06e0612f68ee9fd
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-24 20:58:17 +0900

    improve build script

--- dockerfiles/base/build.sh
+++ dockerfiles/base/build.sh
@@ -2,5 +2,6 @@
 
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64 -t public.ecr.aws/shogo82148/lambda-java:8 "$CURRENT/run"
-docker buildx build --platform linux/amd64 -t public.ecr.aws/shogo82148/lambda-java:build-8 "$CURRENT/build"
+IMAGE_TAG=$(< "$CURRENT/build/Dockerfile" perl -ne 'print $1 if /^FROM\s+(.*)$/')
+docker buildx build --platform linux/amd64 --load -t "$IMAGE_TAG" "$CURRENT/run"
+docker buildx build --platform linux/amd64 --load -t public.ecr.aws/shogo82148/base:build-alami "$CURRENT/build"
