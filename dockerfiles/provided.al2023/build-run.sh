09dbcfc7a38d8dba11cb9b8351ba553a5828f201
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-27 21:27:17 +0900

    provided.al2023: pin the version of base image

---
+++ dockerfiles/provided.al2023/build-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eux
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker buildx build --platform linux/amd64 --load -t lambda-provided:al2023-x86_64 "$CURRENT/run"
+docker buildx build --platform linux/arm64 --load -t lambda-provided:al2023-arm64 "$CURRENT/run"
---
+++ dockerfiles/provided.al2023/update.pl
@@ -0,0 +1,3 @@
+update_image("base.al2023", "run", "base", "al2023");
+update_image("base.al2023", "build", "base", "build-al2023");
+update_init();
