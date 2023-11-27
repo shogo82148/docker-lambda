9183e0d1786824260df300ceed21e8d822dee459
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-27 21:36:30 +0900

    provided.al2: fix tag name

--- dockerfiles/provided.al2/build-build.sh
+++ dockerfiles/provided.al2/build-build.sh
@@ -2,5 +2,5 @@
 
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64 --load -t lambda-provided:build-al2023-x86_64 "$CURRENT/build"
-docker buildx build --platform linux/arm64 --load -t lambda-provided:build-al2023-arm64 "$CURRENT/build"
+docker buildx build --platform linux/amd64 --load -t lambda-provided:build-al2-x86_64 "$CURRENT/build"
+docker buildx build --platform linux/arm64 --load -t lambda-provided:build-al2-arm64 "$CURRENT/build"
