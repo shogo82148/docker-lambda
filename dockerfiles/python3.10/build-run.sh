9ab7a7635193cbc579790c148538b9bc2765a7e6
ICHINOSE Shogo <shogo82148@gmail.com>
2023-11-28 11:29:13 +0900

    python3.10: pin the version of base image

---
+++ dockerfiles/python3.10/build-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eux
+CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker buildx build --platform linux/amd64 --load -t lambda-python:3.10-x86_64 "$CURRENT/run"
+docker buildx build --platform linux/arm64 --load -t lambda-python:3.10-arm64 "$CURRENT/run"
