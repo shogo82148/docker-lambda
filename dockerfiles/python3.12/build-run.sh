fd06f68f9a758dc0c38580a82d7fc87bef21e408
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-15 09:57:34 +0900

    Update Python version to 3.12 in build and run scripts

--- dockerfiles/python3.12/build-run.sh
+++ dockerfiles/python3.12/build-run.sh
@@ -2,5 +2,5 @@
 
 set -eux
 CURRENT=$(cd "$(dirname "$0")" && pwd)
-docker buildx build --platform linux/amd64 --load -t lambda-python:3.11-x86_64 "$CURRENT/run"
-docker buildx build --platform linux/arm64 --load -t lambda-python:3.11-arm64 "$CURRENT/run"
+docker buildx build --platform linux/amd64 --load -t lambda-python:3.12-x86_64 "$CURRENT/run"
+docker buildx build --platform linux/arm64 --load -t lambda-python:3.12-arm64 "$CURRENT/run"
